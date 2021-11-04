f27f436df4b810b2941bc5bbf0159a918348c8f6
CaseyS <sytsemacj@gmail.com>
2021-11-03 22:04:44 -0400

    [Refactored]

--- src/Controller.py
+++ src/Controller.py
@@ -1,44 +1,44 @@
 import Model
-import View
+
 
 class Controller:
     def __init__(self, parent):
         self.parent = parent
         self.model = Model(self)
-        self.view = View(self)
+        
 
-    def get_num_output(input, in_c, out_c):
+    def convert_num(in_c, input,  out_c):
 
-        if in_c == "bin":
-            if out_c == "hex":
+        if in_c == "Binary":
+            if out_c == "Hexadecimal":
                 return Model.bin_2_hex(input)
-            elif out_c == "dec":
+            elif out_c == "Decimal":
                 return Model.bin_2_dec(input)
-            elif out_c == "oct":
+            elif out_c == "Octal":
                 return Model.bin_2_oct(input)
 
-        if in_c == "hex":
-            if out_c == "bin":
+        if in_c == "Hexadecimal":
+            if out_c == "Binary":
                 return Model.hex_2_bin(input)
-            elif out_c == "dec":
+            elif out_c == "Decimal":
                 return Model.hex_2_dec(input)
-            elif out_c == "oct":
+            elif out_c == "Octal":
                 return Model.hex_2_oct(input)
 
-        if in_c == "dec":
-            if out_c == "bin":
+        if in_c == "Decimal":
+            if out_c == "Binary":
                 return Model.dec_2_bin(input)
-            elif out_c == "hex":
+            elif out_c == "Hexadecimal":
                 return Model.dec_2_hex(input)
-            elif out_c == "oct":
+            elif out_c == "Octal":
                 return Model.dec_2_oct(input)
         
-        if in_c == "oct":
-            if out_c == "bin":
+        if in_c == "Octal":
+            if out_c == "Binary":
                 return Model.oct_2_bin(input)
-            elif out_c == "hex":
+            elif out_c == "Hexadecimal":
                 return Model.oct_2_hex(input)
-            elif out_c == "dec":
+            elif out_c == "Decimal":
                 return Model.oct_2_dec(input)
 
 
--- src/View.py
+++ src/View.py
@@ -2,7 +2,7 @@ from tkinter import *
 import tkinter as tk
 from tkinter import font as f
 from tkinter import ttk
-import Controller
+from Controller import Controller
 
 
 #initialize default font
@@ -17,10 +17,11 @@ class App(tk.Tk):
         container.pack(side='top', fill='both', expand=True)
         self.geometry('600x400')
         self.title('Calculator++')
+        self.resizable(False, False)
        
         # initialize frames by stacking them on top of eachother
         self.frames = {}
-        for F in (Menu, NumberConversion, Calculator):
+        for F in (Menu, Numbers):
   
             frame = F(container, self)
             self.frames[F] = frame
@@ -37,39 +38,31 @@ class App(tk.Tk):
 
 #menu class of type Frame
 class Menu(tk.Frame):
-
     def __init__(self, parent, controller):
-
+        title_font = f.Font(family='Helvetica', size=16)
         #initialize menu frame
-        tk.Frame.__init__(self, parent) 
+        tk.Frame.__init__(self, parent)
         self.config(width = 600, height = 400, bg="black")
 
        #title label
-        label = Label(self, text="Calculator++")
-        label.config(font=f.Font(family='Helvetica', size=16), bg='black', fg='white')
-        label.place(relx = .5, rely=.1, anchor='center')
-
+        label = Label(self, text="Calculator++", font=title_font, bg='black', fg="white")
+        label.place(relx =.5, rely = .1, anchor = 'center')
+        
         #formats the buttons
         def menu_buttons(b, h):
             b.config(font=f.Font(family='Helvetica', size=10), bg='black', fg='white', highlightbackground='blue')
             b.place(relx=.5, rely=.5 + h, anchor='center')
         
-        #create buttons
-        calc_button = Button(self, text="Calculator")
-        menu_buttons(calc_button, .1)
-        calc_button.config(command = lambda : controller.show_frame(Calculator))
-        
-        
-        num_button = Button(self, text="Number Conversion")
-        menu_buttons(num_button, .2)
-        num_button.config(command = lambda: controller.show_frame(NumberConversion))
-        
-    
+        num_button = Button(self, text="Number Conversion", command = lambda : controller.show_frame(Numbers))
         unit_button = Button(self, text="Unit Conversion")
-        menu_buttons(unit_button, .3)
+        graph_button = Button(self, text="Graphing")
+
+        menu_buttons(num_button, .1)
+        menu_buttons(unit_button, .2)
+        menu_buttons(graph_button, .3)
 
         
-class NumberConversion(tk.Frame):
+class Numbers(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -77,100 +70,57 @@ class NumberConversion(tk.Frame):
 
         # set options for number choice
         num_options = ["Hex", "Decimal", "Binary", "Octal"]
-        num_chosen = tk.StringVar(self)
-        num_chosen.set("Select Number Type")
-
-        def print_type(choice):
-            chosen = Label(text=choice, font = font)
-            chosen.pack()
+        in_chosen = tk.StringVar(self)
+        in_chosen.set("Select Number Type")
 
         #create drop down menu
-        num_menu = OptionMenu(self, 
-                                                    num_chosen, 
-                                                    *num_options)
-        num_menu.config(height=1, 
-                                       bg="white", 
-                                       text="black", 
-                                       font=font,
-                                       command = print_type(num_chosen.get()))
-
+        num_menu = OptionMenu(self,  in_chosen, *num_options)
+        num_menu.config(height=1, bg="white",  text="black", font=font)
         num_menu.pack(side = tk.LEFT, padx=10)
 
-        input_choice = "None"
-        output_choice = "None"
+        def print_chosen(in_choice, location):
+            choice = Label(text = in_choice, fg = "White")
+            choice.pack(side = tk.LEFT, pady = 10, padx = location)
 
-        input_box = Text(self, height=1, width=16, bg="white") # command = Controller.convert()
-        input_box.pack(side=tk.LEFT,padx=10)
+        print_chosen(in_chosen.get(),  30)
 
+        #create input box
+        input_box = Entry(self, width=16, bg="white")
+        input_box.pack(side=tk.LEFT, padx=10)
+        input = input_box.get()
 
-        num_output_inside = tk.StringVar(self)
-        num_output_inside.set("Select Output Type")
-        num_output_menu = tk.OptionMenu(self, num_output_inside, *num_options)
+        #create output choice menu
+        out_chosen = tk.StringVar(self)
+        out_chosen.set("Select Output Type")
+        num_output_menu = tk.OptionMenu(self, out_chosen, *num_options)
         num_output_menu.config(height=1, bg="white", text="black", font=font)
         num_output_menu.pack(side = tk.LEFT, padx=10)
 
-        output = Text(self, height=1, width =16, bg="white")
-        #output.config(text=Controller.get_num_output(input, input_choice, output_choice))
-        output.pack(side = tk.LEFT, padx= 10)
+
+        output = Controller.convert_num(in_chosen.get(), input, out_chosen.get())
+
+        output_box = Text(self, height=1, width =16, bg="white")
+        output_box.pack(side = tk.LEFT, padx= 10)
+
+        
+
+        convert_button = Button(self, width = 20, 
+                                                font = f.Font(family="Helvetica", size = 12), 
+                                                bg = "grey", 
+                                                fg = "white", 
+                                                text = "Convert",
+                                                command = lambda : show_output()).pack(
+                                                side = tk.BOTTOM, 
+                                                pady = 20)
+        
+        def show_output():
+            output_box.config(text = output)
+
+
 
         #back_button = Button(self, command = App.show_frame(Menu(self)), text="Back To Menu", bg="white", fg="black")
        # back_button.place(rely=.1, relx=.1, anchor='center')
-    
-    
 
-class Calculator(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-
-        def temp():
-            return
-
-        e = Entry(self, font=('Century 26'), width=14, borderwidth=5)
-        e.grid(row=0, column=0, columnspan=4)
-
-        button_1 = Button(self, text="1", width=3, height=2, command=temp)
-        button_2 = Button(self, text="2", width=3, height=2, command=temp)
-        button_3 = Button(self, text="3", width=3, height=2, command=temp)
-        button_4 = Button(self, text="4", width=3, height=2, command=temp)
-        button_5 = Button(self, text="5", width=3, height=2, command=temp)
-        button_6 = Button(self, text="6", width=3, height=2, command=temp)
-        button_7 = Button(self, text="7", width=3, height=2, command=temp)
-        button_8 = Button(self, text="8", width=3, height=2, command=temp)
-        button_9 = Button(self, text="9", width=3, height=2, command=temp)
-        button_0 = Button(self, text="0", width=3, height=2, command=temp)
-        button_add = Button(self, text="+", width=3, height=2, command=temp)
-        button_mult = Button(self, text="x", width=3, height=2, command=temp)
-        button_div = Button(self, text="/", width=3, height=2, command=temp)
-        button_sub = Button(self, text="-", width=3, height=2, command=temp)
-        button_equal = Button(self, text="=", width=3, height=2, command=temp)
-        button_clear = Button(self, text="Clear", width=24, height=2, command=temp)
-        button_dec = Button(self, text=".", width=3, height=2, command=temp)
-
-        #Puts the buttons on the screen
-
-        button_1.grid(row=3, column=0)
-        button_2.grid(row=3, column=1)
-        button_3.grid(row=3, column=2)
-        button_sub.grid(row=3, column=3)
-
-        button_4.grid(row=2, column=0)
-        button_5.grid(row=2, column=1)
-        button_6.grid(row=2, column=2)
-        button_mult.grid(row=2, column=3)
-
-        button_7.grid(row=1, column=0)
-        button_8.grid(row=1, column=1)
-        button_9.grid(row=1, column=2)
-        button_div.grid(row=1, column=3)
-
-        button_0.grid(row=4, column=0)
-        button_dec.grid(row=4, column=1)
-        button_equal.grid(row=4, column=2)
-        button_add.grid(row=4, column=3)
-
-
-        button_clear.grid(row=5, column=0, columnspan=4)
 
 app = App()
 app.mainloop()
