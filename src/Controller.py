e049301cad1cf582547475a1613fe17698eb20d5
CaseyS <sytsemacj@gmail.com>
2021-11-13 21:06:35 -0500

    [Controller Receives]

--- src/Controller.py
+++ src/Controller.py
@@ -2,44 +2,34 @@ import Model
 
 
 class Controller:
-    def __init__(self, parent):
+    def __init__(self, parent, *args, **kwargs):
         self.parent = parent
         self.model = Model(self)
-        
 
+    in_c = ""
+    out_c = ""
+    
     def convert_num(in_c, input,  out_c):
 
         if in_c == "Binary":
-            if out_c == "Hexadecimal":
-                return Model.bin_2_hex(input)
-            elif out_c == "Decimal":
-                return Model.bin_2_dec(input)
-            elif out_c == "Octal":
-                return Model.bin_2_oct(input)
+            if out_c == "Hex": return Model.bin_2_hex(input)
+            elif out_c == "Decimal": return Model.bin_2_dec(input)
+            elif out_c == "Octal": return Model.bin_2_oct(input)
 
-        if in_c == "Hexadecimal":
-            if out_c == "Binary":
-                return Model.hex_2_bin(input)
-            elif out_c == "Decimal":
-                return Model.hex_2_dec(input)
-            elif out_c == "Octal":
-                return Model.hex_2_oct(input)
+        if in_c == "Hex":
+            if out_c == "Binary": return Model.hex_2_bin(input)
+            elif out_c == "Decimal": return Model.hex_2_dec(input)
+            elif out_c == "Octal": return Model.hex_2_oct(input)
 
         if in_c == "Decimal":
-            if out_c == "Binary":
-                return Model.dec_2_bin(input)
-            elif out_c == "Hexadecimal":
-                return Model.dec_2_hex(input)
-            elif out_c == "Octal":
-                return Model.dec_2_oct(input)
+            if out_c == "Binary": return Model.dec_2_bin(input)
+            elif out_c == "Hex": return Model.dec_2_hex(input)
+            elif out_c == "Octal": return Model.dec_2_oct(input)
         
         if in_c == "Octal":
-            if out_c == "Binary":
-                return Model.oct_2_bin(input)
-            elif out_c == "Hexadecimal":
-                return Model.oct_2_hex(input)
-            elif out_c == "Decimal":
-                return Model.oct_2_dec(input)
+            if out_c == "Binary": return Model.oct_2_bin(input)
+            elif out_c == "Hex": return Model.oct_2_hex(input)
+            elif out_c == "Decimal": return Model.oct_2_dec(input)
 
     def selectFile():
         return Model.getFile()
--- src/View.py
+++ src/View.py
@@ -47,6 +47,8 @@ class Menu(tk.Frame):
        #title label
         label = Label(self, text="Calculator++", font=title_font, bg='black', fg="deep sky blue")
         label.place(relx =.5, rely = .1, anchor = 'center')
+
+        default_button_presets = {"fg" : 'deep sky blue', "activebackground" : 'deep sky blue'}
         
         #formats the buttons
         def menu_buttons(b, h):
@@ -55,19 +57,13 @@ class Menu(tk.Frame):
         
         num_button = Button(self, text="Number Conversion", 
                                                              command = lambda : controller.show_frame(Numbers), 
-                                                             activebackground = 'deep sky blue',
-                                                             fg = 'deep sky blue',
-                                                             )
+                                                             **default_button_presets)
         unit_button = Button(self, text="Unit Conversion",
                                                              command = lambda : controller.show_frame(Units), 
-                                                             activebackground = 'deep sky blue',
-                                                             fg = 'deep sky blue',
-                                                             )
+                                                             **default_button_presets)
         graph_button = Button(self, text="Graphing",
                                                              command = lambda : controller.show_frame(Graph),
-                                                             activebackground = 'deep sky blue',
-                                                             fg = 'deep sky blue',
-                                                             )
+                                                             **default_button_presets)
 
         menu_buttons(num_button, .1)
         menu_buttons(unit_button, .2)
@@ -80,105 +76,108 @@ class Numbers(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.config(width = 600, height = 400, bg="black")
 
-        label_font = ("Helvetica", 24)
         blue = 'deep sky blue'
+        label_font = ("Helvetica", 24)
 
-        in_text = Label(self, text = "Input Type",  fg = 'deep sky blue', bg = 'black', font = label_font)
-        in_text.place(rely = .05, relx = .3, anchor = 'center')
+        default_label = {"fg" : blue, "bg" : "black", "font" : label_font}
 
-        out_text = Label(self, text = "Output Type", fg = 'deep sky blue', bg ='black', font = label_font)
-        out_text.place(rely = .05, relx = .725, anchor = 'center')
+        in_text = Label(self, text = "Input Type", **default_label)
+        in_text.place(relx = .3, rely = .05, anchor = 'center')
+        out_text = Label(self, text = "Output Type", **default_label)
+        out_text.place(relx = .725, rely = .05, anchor = 'center')
 
-        binary_button = Button(self, text = "Binary", command = lambda : in_button_clicked(binary_button, "Binary"))
-        hex_button = Button(self, text = "Hex", command = lambda : in_button_clicked(hex_button, "Hex"))
-        decimal_button = Button(self, text = "Decimal", command = lambda : in_button_clicked(decimal_button, "Decimal"))                   
-        octal_button = Button(self, text = "Octal", command = lambda : in_button_clicked(octal_button, "Octal"))
+        button_font = ("Helvetica", 16)
 
-        binary_button2 = Button(self, text = "Binary", command = lambda : out_button_clicked(binary_button2, "Binary"))
-        hex_button2 = Button(self, text = "Hex", command = lambda : out_button_clicked(hex_button2, "Hex"))
-        decimal_button2= Button(self, text = "Decimal", command = lambda : out_button_clicked(decimal_button2, "Decimal"))                   
-        octal_button2 = Button(self, text = "Octal", command = lambda : out_button_clicked(octal_button2, "Octal"))
-
-        # arrange input buttons                       
-        start_height = .15
-        for button in (binary_button, hex_button, decimal_button, octal_button):
-            button.config(fg = 'deep sky blue',
-                                   bg = 'black',
-                                   font = ('Helvetica', 16),
-                                   width = 8,
-                                   activebackground = 'deep sky blue',
-                                   activeforeground = 'black')
-            button.place(relx = .2, rely = start_height)
-            start_height += .12
-
-        # arrange output buttons
-        start_height = .15
-        for button in (binary_button2, hex_button2, decimal_button2, octal_button2):
-            button.config(fg = 'deep sky blue',
-                                   bg = 'black',
-                                   font = ("Helvetica", 16),
-                                   width = 8,
-                                   activebackground = 'deep sky blue',
-                                   activeforeground = 'black')
-            button.place(relx = .625, rely = start_height)
-            start_height += .12
-
+        default_button = {"fg" : blue, 
+                                      "bg" : 'black', 
+                                      "font" : button_font,
+                                      "activeforeground" : "black",
+                                      "activebackground" : blue,
+                                      "width" : 8} 
+        
         input_choice = ""
         output_choice = ""
 
         back_btn = Button(self, 
-                                   text="Back", 
-                                   fg = 'deep sky blue',
-                                   bg = 'black',
-                                   font = ("Helvetica", 16),
-                                   width = 8,
-                                   activebackground = 'deep sky blue',
-                                   activeforeground = 'black',
+                                   **default_button,
+                                   text = "Back",
                                    command = lambda : controller.show_frame(Menu))
         back_btn.place(relx= .1, rely = .05, anchor = 'center')
 
+        # create buttons
+        binary_button = Button(self, text = "Binary", command = lambda : in_button_clicked(binary_button, "Binary"))
+        hex_button = Button(self, text = "Hex", command = lambda : in_button_clicked(hex_button, "Hex"))
+        decimal_button = Button(self, text = "Decimal", command = lambda : in_button_clicked(decimal_button, "Decimal"))
+        octal_button = Button(self, text = "Octal", command = lambda : in_button_clicked(octal_button, "Octal"))
+
+        binary_button2 = Button(self, text = "Binary", command = lambda : out_button_clicked(binary_button2, "Binary"))
+        hex_button2 = Button(self, text = "Hex", command = lambda : out_button_clicked(hex_button2, "Hex"))
+        decimal_button2 = Button(self, text = "Decimal", command = lambda : out_button_clicked(decimal_button2, "Decimal"))
+        octal_button2 = Button(self, text = "Octal", command = lambda : out_button_clicked(octal_button2, "Octal")) 
+        button_names = ["Binary", "Hex", "Decimal", "Octal"]
+
+        current_name = 0
+        # arrange input buttons                       
+        start_height = .15
+        for button in (binary_button, hex_button, decimal_button, octal_button):
+            button.config(**default_button)
+            button.place(relx = .2, rely = start_height)
+            current_name += 1
+            start_height += .12
+
+        # arrange output buttons
+        start_height = .15
+        current_name = 0
+        for button in (binary_button2, hex_button2, decimal_button2, octal_button2):
+            print(button_names[current_name])
+            button.config(**default_button)
+            button.place(relx = .625, rely = start_height)
+            current_name += 1
+            start_height += .12 # vertical distance between buttons
+
+        
         def in_button_clicked(button, choice):
-            button.config(bg = 'deep sky blue', fg = 'black')
-            input_choice = choice
+            button.config(bg = blue, fg = 'black')
+            Controller.in_c = choice
             in_reset_on_click(button)
-            print(input_choice)
+            
         
         def out_button_clicked(button, choice):
-            button.config(bg = 'deep sky blue', fg = 'black')
-            output_choice = choice
+            button.config(bg = blue, fg = 'black')
+            Controller.out_c = choice
             out_reset_on_click(button)
-            print(output_choice)
 
         def in_reset_on_click(chosen):
             for button in (binary_button, hex_button, decimal_button, octal_button):
                 if button is not chosen:
-                    button.config(bg = 'black', fg = 'deep sky blue')
+                    button.config(bg = 'black', fg = blue)
         
         def out_reset_on_click(chosen):
             for button in (binary_button2, hex_button2, decimal_button2, octal_button2):
                 if button is not chosen:
-                    button.config(bg = 'black', fg = 'deep sky blue')
+                    button.config(bg = 'black', fg = blue)
 
-        in_box = Entry(self, bg = 'black', fg = 'deep sky blue', font = ("Helvetica", 16), width = 8)
+        in_box = Entry(self, bg = 'black', fg = blue, font = ("Helvetica", 16), width = 8)
         in_box.place(relx = .2, rely = .8, anchor = 'center')
 
-        out_box = Label(self, bg = 'black', fg = 'deep sky blue', font = ('Helvetica', 16), width = 8, text = "")
+        out_box = Label(self, bg = 'black', fg = blue, font = ('Helvetica', 16), width = 8, text = "Output")
         out_box.place(relx = .8, rely = .8, anchor = 'center')
         
         def convert():
             input = in_box.get()
-            output = Controller.convert_num(input_choice, input, output_choice)
+            output = Controller.convert_num(Controller.in_c, input, Controller.out_c)
+            print(f"Input choice: {Controller.in_c}\ninput: {input}\nOutput Choice: {Controller.out_c}")
             out_box.config(text = output)
             print(output)
 
         convert_button = Button(self,
                                                  text = "Convert",
-                                                 fg = 'deep sky blue',
+                                                 fg = blue,
                                                  bg = 'black',
                                                  font = ("Helvetica", 16),
                                                  width = 7,
                                                  height = 1,
-                                                 activebackground = 'deep sky blue',
+                                                 activebackground = blue,
                                                  activeforeground = 'black',
                                                  command = lambda : convert())
 
@@ -198,7 +197,7 @@ class Units(tk.Frame):
         self.config(width = 600, height = 400, bg="black")
 
 
-
+        
 
         back_btn = Button(self, 
                                    text="Back", 
