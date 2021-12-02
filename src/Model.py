0b7b8551d3d58bfc9258eec3db795fa005fd955a
Lucas Johnson <johnsluc@mail.gvsu.edu>
2021-12-01 21:37:45 -0500

    [Adds Histogram and 3 Variable Graphing functions]

--- src/Model.py
+++ src/Model.py
@@ -147,7 +147,6 @@ def selectUnits():
         global graph_option
 
         option = IntVar()
-        option.set("1")
         value=1
 
         def clicked(option):
@@ -162,7 +161,7 @@ def selectUnits():
         option1 = Radiobutton(newWindow, text="2 Variable Graph", variable=option, value=1, command=lambda: clicked(option.get()), background="black")
         option1.pack()
 
-        option2 = Radiobutton(newWindow, text="Multi-Variable Graph", variable=option, value=2, command=lambda: clicked(option.get()), background="black")
+        option2 = Radiobutton(newWindow, text="3 Variable Graph", variable=option, value=2, command=lambda: clicked(option.get()), background="black")
         option2.pack()
         
         option3 = Radiobutton(newWindow, text="Histogram",variable=option, value=3, command=lambda: clicked(option.get()), background="black")
@@ -223,36 +222,45 @@ def selectUnits():
 
             newWindow = tk.Toplevel()
             newWindow.title("Select Units")
-            newWindow.geometry('210x90')
-            newWindow.maxsize(207,80)
-            newWindow.minsize(207,80)
+            newWindow.geometry('2107x105')
+            newWindow.maxsize(207,105)
+            newWindow.minsize(207,105)
             newWindow.config(bg='black')
 
             values1 = StringVar(newWindow)
             values2 = StringVar(newWindow)
+            values3 = StringVar(newWindow)
 
             values1.set(header[0])
             values2.set(header[0])
+            values3.set(header[0])
 
             options1 = OptionMenu(newWindow, values1, *unitOptions)
             options1.config(width=6, bg="black")
             options2 = OptionMenu(newWindow, values2, *unitOptions)
             options2.config(width=6, bg="black")
+            options3 = OptionMenu(newWindow, values3, *unitOptions)
+            options3.config(width=6, bg="black")
 
             options1.grid(row=0,column=1)
             options2.grid(row=1,column=1)
+            options3.grid(row=2,column=1)
 
             Entry1 = Label(newWindow, width = 10, justify="left", text="Variable 1 (X): ", background="black")
             Entry1.grid(row=0,column=0)
-            Entry2 = Label(newWindow, width = 10, justify="left", text="Variable 2 (Y): ", background="black")
+            Entry2 = Label(newWindow, width = 10, justify="left", text="Variable 2 (Y1): ", background="black")
             Entry2.grid(row=1,column=0)
+            Entry3 = Label(newWindow, width = 10, justify="left", text="Variable 3 (Y2): ", background="black")
+            Entry3.grid(row=2,column=0)
 
             def submit():
                 global variable1
                 global variable2
+                global variable3
 
                 variable1 = values1.get()
                 variable2 = values2.get()
+                variable3 = values3.get()
 
                 newWindow.destroy()
 
@@ -263,7 +271,7 @@ def selectUnits():
                 highlightbackground="black",
                 text="Submit", 
                 command=submit)
-            submitButton.grid(row=2,columnspan=2)
+            submitButton.grid(row=3,columnspan=2)
 
 
         if graph_option ==3:
@@ -312,7 +320,7 @@ def selectUnits():
                 text="Submit", 
                 command=submit)
             submitButton.grid(row=2,columnspan=2)
-            
+
     getGraph()
 
 #Shows graph based of user input file and selected Units
@@ -320,6 +328,7 @@ def graph():
 
     global variable1
     global variable2
+    global variable3
     global graph_option
 
 
@@ -373,10 +382,161 @@ def graph():
         plt.show()
 
     def graph_2():
-        print("Option 2")
+
+        index1 = header.index(variable1) + 1
+        index2 = header.index(variable2) + 1
+        index3 = header.index(variable3) + 1
+
+        list1 = vars[index1]
+        list2 = vars[index2]
+        list3 = vars[index3]
+
+
+        list1 = list(map(int, list1))
+        list2 = list(map(int, list2))
+        list3 = list(map(int, list3))
+
+        max1 = 0
+        max2 = 0
+        max3 = 0
+        
+        min1 = list1[0]
+        min2 = list2[0]
+        min3 = list3[0]
+
+        for item in list1:
+            if item > max1:
+                max1 = item
+            if item < min1:
+                min1 = item
+
+        for item in list2:
+            if item > max2:
+                max2 = item
+            if item < min2:
+                min2 = item
+
+        for item in list3:
+            if item > max3:
+                max3 = item
+            if item < min3:
+                min3 = item
+
+        if(max3 > max2):
+            if(max3 < 20):
+                max3=max3+1
+            elif(max3 < 100):
+                max3=max3+5
+            elif(max3 > 100):
+                max3=max3+20
+            elif(max3 > 1000):
+                max3=max3+50
+        else:
+            if(max2 < 20):
+                max2=max2+1
+            elif(max2 < 100):
+                max2=max2+5
+            elif(max2 > 100):
+                max2=max2+20
+            elif(max2 > 1000):
+                max2=max2+50
+        
+
+        if(variable2==variable3):
+            plt.figure("Graph")
+            plt.plot(list1, list2, '-bo')
+
+            plt.xlabel(str(variable1))
+            plt.ylabel(str(variable2))
+
+            plt.axis([min1,max1,0,max2])
+
+
+            plt.show()
+        else:
+            plt.figure("Graph")
+            plt.scatter(list1, list2, marker = 'o', color='b', label=str(variable2))
+            plt.scatter(list1, list3, marker='o', color='r', label=str(variable3))
+            plt.plot(list1, list2, '-', list1, list3, '-')
+
+            plt.xlabel(str(variable1))
+            plt.ylabel(str(variable2) + " and " + str(variable3))
+
+            if(max3 > max2):
+                plt.axis([min1,max1,0,max3])
+            else:
+                plt.axis([min1,max1,0,max2])
+
+            plt.legend()
+            plt.show()
 
     def graph_3():
-        print("Option 3")
+        index1 = header.index(variable1) + 1
+        index2 = header.index(variable2) + 1
+
+        list1 = vars[index1]
+        list2 = vars[index2]
+
+
+        list1 = list(map(int, list1))
+        list2 = list(map(int, list2))
+
+        max1 = 0
+        max2 = 0
+        
+        min1 = list1[0]
+        min2 = list2[0]
+
+        for item in list1:
+            if item > max1:
+                max1 = item
+            if item < min1:
+                min1 = item
+
+        for item in list2:
+            if item > max2:
+                max2 = item
+            if item < min2:
+                min2 = item
+
+        if(min1 < 20):
+            min1=min1-1
+        elif(min1 < 100):
+            min1=min1-5
+        elif(min1 > 100):
+            min1=min1-20
+        elif(min1 > 1000):
+            min1=min1-50
+
+        if(max1 < 20):
+            max1=max1+1
+        elif(max1 < 100):
+            max1=max1+5
+        elif(max1 > 100):
+            max1=max1+20
+        elif(max1 > 1000):
+            max1=max1+50
+
+
+        if(max2 < 20):
+            max2=max2+1
+        elif(max2 < 100):
+            max2=max2+5
+        elif(max2 > 100):
+            max2=max2+20
+        elif(max2 > 1000):
+            max2=max2+50
+        
+        plt.figure("Graph")
+        plt.bar(list1, list2)
+
+        plt.xlabel(str(variable1))
+        plt.ylabel(str(variable2))
+
+        plt.axis([min1,max1,0,max2])
+
+
+        plt.show()
 
     if graph_option == 1:
         graph_1()
